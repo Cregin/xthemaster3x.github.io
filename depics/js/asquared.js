ca0a96f079c328cbafcea8bcd816a81a312fc2a1
xTheMaster3x <xTheMaster3x@users.noreply.github.com>
2015-08-03 13:15:28 -0400

    Well that didn't work at all...

--- depics/js/asquared.js
+++ depics/js/asquared.js
@@ -10,17 +10,11 @@ function iOSversion() {
 ver = iOSversion();
 var block = document.getElementById("version");
 var version = iOSversion();
-if (ver[0] = 8) {
+if (ver[0] >= 8) {
 	block.setAttribute('style', "background-color:#dfd");
 	block.innerHTML='<p>Your device <strong>is</strong> supported.</p>';
 }
 else {
-	if (ver[0] > 8) {
-		block.setAttribute('style', "background-color:#ffd");
-		block.innerHTML='<p>Your device <strong>may not</strong> be supported.</p><hr><p>This is currently untested on iOS '+ver[0]+'.  But you can test it and let me know!</p>';
-	}
-	else {
-		block.setAttribute('style', "background-color:#ddf");
-		block.innerHTML='<p>Your device is not supported.</p>';
-	}
+	block.setAttribute('style', "background-color:#ffd");
+	block.innerHTML='<p>Your device <strong>may not</strong> be supported.</p><hr><p>This is currently untested on iOS '+ver[0]+'.  But you can test it and let me know!</p>';
 }
